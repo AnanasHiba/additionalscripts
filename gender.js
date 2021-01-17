1d7900241ec7d2c1e4ff508ee531f6e178eeb0df
AnanasHiba <lynnyk_vova@ukr.net>
2021-01-17 19:07:52 +0200

    Update gender.js

--- gender.js
+++ gender.js
@@ -2,27 +2,6 @@ var fs = require("fs");
 var request = require("request");
 var mv = require("mv");
 
-/**var options = { method: 'POST',
-  url: 'https://api.enface.ai/v1/detect',
-  headers: 
-   { 'postman-token': '8e1cb668-d525-9b63-3a45-7f6c5acbac4a',
-     'cache-control': 'no-cache',
-     'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' },
-  formData: 
-   { images: 
-      { value: fs.createReadStream("/home/volodymyr/pics/15.jpg"),
-        options: 
-         { filename: '15.jpg',
-           contentType: null } },
-     token: '722cbb30-15ff-11eb-a983-d341f34204e0',
-     feature_check: 'gender' } };
-
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
-
-  console.log(body);
-});**/
-
 var file = "/home/volodymyr/Documents/res/";
 
 fs.readdir( file + "new/", async (err, files) => {
